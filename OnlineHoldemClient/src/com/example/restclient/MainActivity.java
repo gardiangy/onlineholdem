b518f45427010687baf86b9406c68fa51f3d94b3
gardiangy <gardiangy@gmail.com>
2013-07-04 21:36:57 +0200

    skeleton client for testing rest

---
+++ OnlineHoldemClient/src/com/example/restclient/MainActivity.java
@@ -0,0 +1,278 @@
+package com.example.restclient;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class MainActivity extends Activity {
+
+    private static final String SERVICE_URL = "http://80.98.252.51:8080/OnlineHoldemServer/rest";
+
+    private static final String TAG = "MainActivity";
+
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SeekBar betBar = (SeekBar) findViewById(R.id.chipBar);
+        betBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                Button betBtn = (Button) findViewById(R.id.btnBet);
+                betBtn.setText("Bet " + i);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    public void retrieveSampleData(View vw) {
+
+        String sampleURL = SERVICE_URL + "/message";
+
+        WebServiceTask wst = new WebServiceTask(WebServiceTask.GET_TASK, this,
+                "GETting data...");
+
+        wst.execute(new String[]{sampleURL});
+
+
+    }
+
+    public void postData(View vw) {
+
+        String postURL = SERVICE_URL + "/";
+
+        EditText message = (EditText) findViewById(R.id.editText);
+
+        WebServiceTask wst = new WebServiceTask(WebServiceTask.POST_TASK, this,
+                "Posting data...");
+
+        wst.addNameValuePair("message", message.getText().toString());
+
+        // the passed String is the URL we will POST to
+        wst.execute(new String[]{postURL});
+
+
+    }
+
+    public void handleResponse(String response) {
+
+        EditText message = (EditText) findViewById(R.id.editText);
+
+        message.setText("");
+
+        TextView view = (TextView) findViewById(R.id.textView);
+
+        view.setText(response);
+
+
+    }
+
+    private void hideKeyboard() {
+
+        InputMethodManager inputManager = (InputMethodManager) MainActivity.this
+                .getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        inputManager.hideSoftInputFromWindow(MainActivity.this
+                .getCurrentFocus().getWindowToken(),
+                InputMethodManager.HIDE_NOT_ALWAYS);
+    }
+
+    private class WebServiceTask extends AsyncTask<String, Integer, String> {
+
+        public static final int POST_TASK = 1;
+        public static final int GET_TASK = 2;
+
+        private static final String TAG = "WebServiceTask";
+
+        // connection timeout, in milliseconds (waiting to connect)
+        private static final int CONN_TIMEOUT = 3000;
+
+        // socket timeout, in milliseconds (waiting for data)
+        private static final int SOCKET_TIMEOUT = 5000;
+
+        private int taskType = GET_TASK;
+        private Context mContext = null;
+        private String processMessage = "Processing...";
+
+        private ArrayList<NameValuePair> params = new ArrayList<NameValuePair>();
+
+        private ProgressDialog pDlg = null;
+
+        public WebServiceTask(int taskType, Context mContext,
+                              String processMessage) {
+
+            this.taskType = taskType;
+            this.mContext = mContext;
+            this.processMessage = processMessage;
+        }
+
+        public void addNameValuePair(String name, String value) {
+
+            params.add(new BasicNameValuePair(name, value));
+        }
+
+        private void showProgressDialog() {
+
+            pDlg = new ProgressDialog(mContext);
+            pDlg.setMessage(processMessage);
+            pDlg.setProgressDrawable(mContext.getWallpaper());
+            pDlg.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            pDlg.setCancelable(false);
+            pDlg.show();
+
+        }
+
+        @Override
+        protected void onPreExecute() {
+
+            hideKeyboard();
+            showProgressDialog();
+
+        }
+
+        protected String doInBackground(String... urls) {
+
+            String url = urls[0];
+            String result = "";
+
+            HttpResponse response = doResponse(url);
+
+
+            if (response == null) {
+                return result;
+            } else {
+
+                try {
+
+                    result = inputStreamToString(response.getEntity()
+                            .getContent());
+
+                } catch (IllegalStateException e) {
+                    Log.e(TAG, e.getLocalizedMessage(), e);
+
+                } catch (IOException e) {
+                    Log.e(TAG, e.getLocalizedMessage(), e);
+                }
+
+            }
+
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String response) {
+
+            handleResponse(response);
+            pDlg.dismiss();
+
+        }
+
+        // Establish connection and socket (data retrieval) timeouts
+        private HttpParams getHttpParams() {
+
+            HttpParams htpp = new BasicHttpParams();
+
+            HttpConnectionParams.setConnectionTimeout(htpp, CONN_TIMEOUT);
+            HttpConnectionParams.setSoTimeout(htpp, SOCKET_TIMEOUT);
+
+            return htpp;
+        }
+
+        private HttpResponse doResponse(String url) {
+
+            // Use our connection and data timeouts as parameters for our
+            // DefaultHttpClient
+            HttpClient httpclient = new DefaultHttpClient(getHttpParams());
+
+            HttpResponse response = null;
+
+            try {
+                switch (taskType) {
+
+                    case POST_TASK:
+                        HttpPost httppost = new HttpPost(url);
+                        // Add parameters
+                        httppost.setEntity(new UrlEncodedFormEntity(params));
+
+                        response = httpclient.execute(httppost);
+                        break;
+                    case GET_TASK:
+                        HttpGet httpget = new HttpGet(url);
+                        response = httpclient.execute(httpget);
+                        break;
+                }
+            } catch (Exception e) {
+
+                Log.e(TAG, e.getLocalizedMessage(), e);
+
+            }
+
+            return response;
+        }
+
+        private String inputStreamToString(InputStream is) {
+
+            String line = "";
+            StringBuilder total = new StringBuilder();
+
+            // Wrap a BufferedReader around the InputStream
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+
+            try {
+                // Read response until the end
+                while ((line = rd.readLine()) != null) {
+                    total.append(line);
+                }
+            } catch (IOException e) {
+                Log.e(TAG, e.getLocalizedMessage(), e);
+            }
+
+            // Return full string
+            return total.toString();
+        }
+    }
+
+}
