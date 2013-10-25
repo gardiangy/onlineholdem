4329548b404dbe4e8cae8dcce5aad7597bfa21dc
gardiangy <gardiangy@gmail.com>
2013-10-25 20:19:19 +0200

    deal animation

--- OnlineHoldemClient/src/hu/onlineholdem/restclient/activity/SinglePlayerActivity.java
+++ OnlineHoldemClient/src/hu/onlineholdem/restclient/activity/SinglePlayerActivity.java
@@ -5,9 +5,11 @@ import android.graphics.Point;
 import android.os.Bundle;
 import android.view.Display;
 import android.view.View;
+import android.view.Window;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -32,14 +34,21 @@ public class SinglePlayerActivity extends Activity{
     private ImageView flop1;
     private ImageView flop2;
     private ImageView flop3;
+    private ImageView turn;
+    private ImageView river;
+    private RelativeLayout board;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.single_player_layout);
         flop1 = (ImageView) findViewById(R.id.flop1);
         flop2 = (ImageView) findViewById(R.id.flop2);
         flop3 = (ImageView) findViewById(R.id.flop3);
+        turn = (ImageView) findViewById(R.id.turn);
+        river = (ImageView) findViewById(R.id.river);
+        board = (RelativeLayout) findViewById(R.id.board);
         Display display = getWindowManager().getDefaultDisplay();
         Point size = new Point();
         display.getSize(size);
@@ -54,6 +63,15 @@ public class SinglePlayerActivity extends Activity{
                 deck.add(card);
             }
         }
+        TextView player1 = new TextView(this);
+        player1.setBackgroundResource(R.drawable.seatnotactive);
+
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(200,100);
+        layoutParams.setMargins(60,50,0,0);
+        player1.setLayoutParams(layoutParams);
+
+
+        board.addView(player1);
 
         SeekBar betBar = (SeekBar) findViewById(R.id.betBar);
         betBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -77,7 +95,7 @@ public class SinglePlayerActivity extends Activity{
 
 
     }
-    public void deal(View view){
+    public void dealFlop(View view){
         Collections.shuffle(deck,new Random());
         int resId = getResources().getIdentifier(deck.get(0).toString(), "drawable", this.getPackageName());
         int res2Id = getResources().getIdentifier(deck.get(1).toString(), "drawable", this.getPackageName());
@@ -87,16 +105,64 @@ public class SinglePlayerActivity extends Activity{
         flop1.setAnimation(flop1Anim);
         flop1.setImageResource(resId);
         flop1.startAnimation(flop1Anim);
+        deck.remove(0);
 
         Animation flop2Anim = createAnimation(screenWidth/2 - 200,180);
         flop2.setAnimation(flop2Anim);
         flop2.setImageResource(res2Id);
         flop2.startAnimation(flop2Anim);
+        deck.remove(1);
 
         Animation flop3Anim = createAnimation(screenWidth/2 - 100,180);
         flop3.setAnimation(flop3Anim);
         flop3.setImageResource(res3Id);
         flop3.startAnimation(flop3Anim);
+        deck.remove(2);
+
+    }
+
+    public void dealTurn(View view){
+        int resId = getResources().getIdentifier(deck.get(0).toString(), "drawable", this.getPackageName());
+
+        Animation turnAnim = createAnimation(screenWidth/2,180);
+        turn.setAnimation(turnAnim);
+        turn.setImageResource(resId);
+        turn.startAnimation(turnAnim);
+        deck.remove(0);
+
+    }
+
+    public void dealRiver(View view){
+        int resId = getResources().getIdentifier(deck.get(0).toString(), "drawable", this.getPackageName());
+
+        Animation riverAnim = createAnimation(screenWidth/2 + 100,180);
+        river.setAnimation(riverAnim);
+        river.setImageResource(resId);
+        river.startAnimation(riverAnim);
+        deck.remove(0);
+
+    }
+
+    public void deal(View view){
+        int resId = getResources().getIdentifier(deck.get(0).toString(), "drawable", this.getPackageName());
+        int res2Id = getResources().getIdentifier(deck.get(1).toString(), "drawable", this.getPackageName());
+
+        TextView player1 = (TextView)board.getChildAt(board.getChildCount() - 1);
+        Animation card1Anim = createAnimation(player1.getWidth()/2,player1.getHeight() - 80);
+        ImageView card1 = new ImageView(this);
+        board.addView(card1,board.getChildCount() - 2);
+        card1.setAnimation(card1Anim);
+        card1.setImageResource(resId);
+        card1.startAnimation(card1Anim);
+
+        Animation card2Anim = createAnimation(player1.getWidth()/2 + 40,player1.getHeight() - 80);
+        ImageView card2 = new ImageView(this);
+        board.addView(card2,board.getChildCount() - 2);
+        card2.setAnimation(card2Anim);
+        card2.setImageResource(res2Id);
+        card2.startAnimation(card2Anim);
+        deck.remove(0);
+        deck.remove(1);
 
     }
 
